effa86cca536e79e1fc70e936e89cde7eafca981
piotrmizerka <piotrmizerka@gmail.com>
2021-12-15 16:22:49 +0100

    Turn project into a package.

--- src/LowCohomologySOS.jl
+++ src/LowCohomologySOS.jl
@@ -1,3 +1,5 @@
+module LowCohomologySOS
+
 using LinearAlgebra
 ENV["JULIA_NUM_THREADS"] = 4
 LinearAlgebra.BLAS.set_num_threads(2)
@@ -15,3 +17,5 @@ include("group_rings.jl")
 include("fox_derivatives.jl")
 include("positive_approx.jl")
 include("certification.jl")
+
+end
