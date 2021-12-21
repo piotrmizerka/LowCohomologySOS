0e75d895e4e2c1883d5bbf83c1b05700fb1715a9
piotrmizerka <piotrmizerka@gmail.com>
2021-12-21 23:23:37 +0100

    Tests for each put into separate files - each file for each script file

---
+++ test/group_rings_tests.jl
@@ -0,0 +1,59 @@
+@testset "cyclic_group" begin 
+    n = 3
+    Cₙ = LowCohomologySOS.cyclic_group(n)
+    a, = Groups.gens(Cₙ)
+ 
+    @test a^n == one(Cₙ)
+    @test all(a^i*a^j == a^(i+j) for i in 2:n for j in 2:n)
+ end
+ 
+ @testset "group_ring" begin 
+    n = 15
+    Cₙ = LowCohomologySOS.cyclic_group(n)
+    a, = Groups.gens(Cₙ)
+ 
+    RCₙ = LowCohomologySOS.group_ring(Cₙ, n)
+    b = RCₙ.basis
+    mstr = RCₙ.mstructure
+    @test RCₙ.object == Cₙ
+    @test Set(collect(b)) == Set([[a];[a^i for i in 2:n]])
+    @test mstr[getindex(b, a),getindex(b, a)] == getindex(b, a^2)
+    @test all(mstr[getindex(b, a),getindex(b, a^j)] == getindex(b, a^(j+1)) for j in 2:n)
+    @test all(mstr[getindex(b, a^i),getindex(b, a^j)] == getindex(b, a^(i+j)) for i in 2:n for j in 2:n)
+ 
+    RCₙ_star = LowCohomologySOS.group_ring(Cₙ, n, true)
+    b_star = RCₙ_star.basis
+    mstr_star = RCₙ_star.mstructure
+    @test RCₙ_star.object == Cₙ
+    @test Set(collect(b_star)) == Set([[a];[a^i for i in 2:n]])
+    @test mstr_star[getindex(b_star, a),getindex(b_star, a)] == getindex(b_star, one(Cₙ))
+    @test all(mstr_star[getindex(b_star, a),getindex(b_star, a^j)] == getindex(b_star, a^(-1+j)) for j in 2:n if -1+j != 1)
+    @test all(mstr_star[getindex(b_star, a^i),getindex(b_star, a^j)] == getindex(b_star, a^(-i+j)) for i in 2:n for j in 2:n if -i+j != 1 && i-j != 1)
+ 
+    A = Alphabet([:x, :X, :y, :Y, :z, :Z], [2, 1, 4, 3, 6, 5])
+    F = FreeGroup(A)
+    x, y, z = Groups.gens(F)
+    half_basis = [x, x*y*z]
+ 
+    RF = LowCohomologySOS.group_ring(F, half_basis)
+    b_RF = RF.basis
+    mstr_RF = RF.mstructure
+    @test RF.object == F
+    @test Set(collect(b_RF)) == Set([one(F), x, x^(-1), x*y*z, (x*y*z)^(-1),
+                                         x^2, x^(-2), (x*y*z)^2, (x*y*z)^(-2),
+                                         x^2*y*z, x*(x*y*z)^(-1), y*z, x^(-1)*(x*y*z)^(-1),
+                                         x*y*z*x, x*y*z*x^(-1), (x*y*z)^(-1)*x, (x*y*z)^(-1)*x^(-1)])                                    
+    @test mstr_RF[getindex(b_RF, x),getindex(b_RF, x*y*z)] == getindex(b_RF, x^2*y*z)
+    @test mstr_RF[getindex(b_RF, x*y*z),getindex(b_RF, x*y*z)] == getindex(b_RF, (x*y*z)^2)
+    @test mstr_RF[getindex(b_RF, (x*y*z)^(-1)),getindex(b_RF, x^(-1))] == getindex(b_RF, (x*y*z)^(-1)*x^(-1))
+    
+    RF_star = LowCohomologySOS.group_ring(F, half_basis, true)
+    b_RF_star = RF_star.basis
+    mstr_RF_star = RF_star.mstructure
+    @test RF_star.object == F 
+    @test Set(collect(b_RF_star)) == Set(collect(b_RF))                                  
+    @test mstr_RF_star[getindex(b_RF_star, x),getindex(b_RF_star, x*y*z)] == getindex(b_RF_star, y*z)
+    @test mstr_RF_star[getindex(b_RF_star, x*y*z),getindex(b_RF_star, x*y*z)] == getindex(b_RF_star, one(F))
+    @test mstr_RF_star[getindex(b_RF_star, (x*y*z)^(-1)),getindex(b_RF_star, x^(-1))] == getindex(b_RF_star, x*y*z*x^(-1))
+ end
+ 
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,61 +2,10 @@ using StarAlgebras
 using Groups
 using Test
 using LowCohomologySOS
+using AbstractAlgebra
+using Suppressor
 
-
-@testset "cyclic_group" begin n = 3
-   Cₙ = LowCohomologySOS.cyclic_group(n)
-
-   a, = Groups.gens(Cₙ)
-
-   @test a^n == one(Cₙ)
-   @test all(a^i*a^j == a^(i+j) for i in 2:n for j in 2:n)
-end
-
-@testset "group_ring" begin n = 15
-   Cₙ = LowCohomologySOS.cyclic_group(n)
-   a, = Groups.gens(Cₙ)
-
-   RCₙ = LowCohomologySOS.group_ring(Cₙ, n)
-   @test RCₙ.object == Cₙ
-   @test Set(collect(RCₙ.basis)) == Set([[a];[a^i for i in 2:n]])
-   mstr = RCₙ.mstructure
-   b = RCₙ.basis
-   @test mstr[getindex(b, a),getindex(b, a)] == getindex(b, a^2)
-   @test all(mstr[getindex(b, a),getindex(b, a^j)] == getindex(b, a^(j+1)) for j in 2:n)
-   @test all(mstr[getindex(b, a^i),getindex(b, a^j)] == getindex(b, a^(i+j)) for i in 2:n for j in 2:n)
-
-   RCₙ_star = LowCohomologySOS.group_ring(Cₙ, n, true)
-   @test RCₙ_star.object == Cₙ
-   @test Set(collect(RCₙ_star.basis)) == Set([[a];[a^i for i in 2:n]])
-   mstr_star = RCₙ_star.mstructure
-   b_star = RCₙ_star.basis
-   @test mstr_star[getindex(b_star, a),getindex(b_star, a)] == getindex(b_star, one(Cₙ))
-   @test all(mstr_star[getindex(b_star, a),getindex(b_star, a^j)] == getindex(b_star, a^(-1+j)) for j in 2:n if -1+j != 1)
-   @test all(mstr_star[getindex(b_star, a^i),getindex(b_star, a^j)] == getindex(b_star, a^(-i+j)) for i in 2:n for j in 2:n if -i+j != 1 && i-j != 1)
-end
-
-@testset "fox_derivative" begin
-   A = Alphabet([:x, :X, :y, :Y], [2, 1, 4, 3])
-   F = FreeGroup(A)
-   RF = LowCohomologySOS.group_ring(F, 2)
-
-   @test LowCohomologySOS.fox_derivative(RF, one(F), 1) == zero(RF)
-   @test LowCohomologySOS.fox_derivative(RF, one(F), 2) == zero(RF)
-
-   x, y = Groups.gens(F)
-
-   @test LowCohomologySOS.fox_derivative(RF, x, 1) == one(RF)
-   @test LowCohomologySOS.fox_derivative(RF, x, 2) == zero(RF)
-   @test LowCohomologySOS.fox_derivative(RF, y, 1) == zero(RF)
-   @test LowCohomologySOS.fox_derivative(RF, y, 2) == one(RF)
-
-   @test LowCohomologySOS.fox_derivative(RF, inv(x), 1) == -RF(inv(x))
-   @test LowCohomologySOS.fox_derivative(RF, inv(y), 2) == -RF(inv(y))
-
-   @test LowCohomologySOS.fox_derivative(RF, x*y, 1) == one(RF)
-   @test LowCohomologySOS.fox_derivative(RF, x*y, 2) == RF(x)
-
-   @test LowCohomologySOS.fox_derivative(RF, x*y*x*y, 1) == one(RF)+RF(x*y)
-   @test LowCohomologySOS.fox_derivative(RF, x*y*x*y, 2) == RF(x)+RF(x*y*x)
+@testset "LowCohomologySOS" begin
+   include("group_rings_tests.jl")
+   include("fox_derivatives_tests.jl")
 end
