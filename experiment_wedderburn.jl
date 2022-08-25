7541dce3de982e48aebcc7b7f3d4167893e9a0a4
piotrmizerka <piotrmizerka@gmail.com>
2022-08-25 16:36:06 +0200

    symmetrized matrix reconstruction
    caveat: unregistered PropertyT-new added - remember to add it yourself

--- experiment_wedderburn.jl
+++ experiment_wedderburn.jl
@@ -15,12 +15,15 @@ struct Word{T}
     end
 end
 Base.show(io::IO, w::Word) = join(io, w.alphabet[w.letters], "·")
-
 Base.:(==)(w::Word, v::Word) =
     w.alphabet == v.alphabet && w.letters == v.letters
 Base.hash(w::Word, h::UInt = UInt(0)) =
     hash(w.alphabet, hash(w.letters, hash(Word, h)))
 
+# defining the symmetry group Σ
+Σ = S₃ = PermGroup(perm"(1,2,3)", perm"(1,2)")
+
+# defining the action (by permutations in this case)
 struct OnLetters <: SymbolicWedderburn.ByPermutations end
 function SymbolicWedderburn.action(
     ::OnLetters,
@@ -29,7 +32,9 @@ function SymbolicWedderburn.action(
 )
     return Word(w.alphabet, [l^p for l in w.letters])
 end
+action = OnLetters()
 
+# defining the set on which Σ acts
 E = words = let A = [:x, :y, :z], radius = 1
     words = [Word(A, [1]), Word(A, [2]), Word(A, [3])]
     for r in 2:radius
@@ -44,10 +49,6 @@ E = words = let A = [:x, :y, :z], radius = 1
     words
 end
 
-Σ = S₃ = PermGroup(perm"(1,2,3)", perm"(1,2)") # Σ will act on words permuting letters
-
-action = OnLetters()
-
 sa = SymbolicWedderburn.symmetry_adapted_basis(Rational{Int}, Σ, action, E)
 
 mπs = SymbolicWedderburn.multiplicity.(sa)
@@ -55,3 +56,34 @@ mπs = SymbolicWedderburn.multiplicity.(sa)
 wdec = SymbolicWedderburn.WedderburnDecomposition(Float64, Σ, action, E, E)
 
 Uπs = direct_summands(wdec)
+
+pairs(Uπs)
+
+# let's compute the invariant matrix from sample block ones (see the notes pp. 9 and 10)
+using PropertyT_new
+
+# Uπs - just to compare with the notes (the may be rescaled), we don't need them further, however, as the whole computation
+# is enclosed in the "reconstruct" function from the PropertyT-new package
+U_trivial = Uπs[2]    
+U₂ = Uπs[1]
+
+# first reconstruction example
+P_trivial = [1]
+P₂ = [1]
+P_blocks = [P₂, P_trivial] # I noticed the order of direct summands is non-standard here
+
+P_invariant = PropertyT_new.reconstruct(P_blocks, wdec)
+
+# second reconstruction example
+P_trivial = [1]
+P₂ = [2]
+P_blocks = [P₂, P_trivial] # I noticed the order of direct summands is non-standard here
+
+P_invariant = PropertyT_new.reconstruct(P_blocks, wdec)
+
+# third reconstruction example
+P_trivial = [10]
+P₂ = [1]
+P_blocks = [P₂, P_trivial] # I noticed the order of direct summands is non-standard here
+
+P_invariant = PropertyT_new.reconstruct(P_blocks, wdec)
