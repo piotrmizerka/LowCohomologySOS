1984c4dd94ebe6beb063fd5df1ac3c1da370f036
piotrmizerka <piotrmizerka@gmail.com>
2022-10-17 12:00:22 +0200

    still, the constraint matrix has to be invariant
    bugs with half_basis fixed

--- test/positive_approx_symmetrized_tests.jl
+++ test/positive_approx_symmetrized_tests.jl
@@ -4,8 +4,8 @@
         [s; inv.(s)]
     end
     S = unique!(S)
-    half_basis = S
     basis, sizes = Groups.wlmetric_ball(S, radius = 2)
+    half_basis = basis[1:sizes[1]]
     _conj = LowCohomologySOS._conj
     ℝSAutF₂_star = LowCohomologySOS.group_ring(SAutF₂, half_basis, star_multiplication = true)
     cnstrs = LowCohomologySOS.constraints(ℝSAutF₂_star.mstructure)
@@ -26,8 +26,25 @@
         A_gs,
         length(S)
     )
-
     @test size(inv_cnstr_matrix) == (length(psd_basis), length(psd_basis))
+    # check invariance of inv_cnstr_matrix: this test is failed - study the invariance!
+    half_basis_idies = Dict(half_basis[i] => i for i in 1:length(half_basis))
+    for i in 1:length(S)
+        for j in 1:length(S)
+            inv_cnstr_matrix_i_j = @view inv_cnstr_matrix[LowCohomologySOS.KroneckerDelta{length(S)}(i, j)]
+            for σ ∈ Σ
+                σ_tse = SymbolicWedderburn.action(action, σ, LowCohomologySOS.TensorSupportElement(S[i], S[j], first(S)))
+                σi, σj = word(σ_tse.row_generator)[1], word(σ_tse.column_generator)[1]
+                inv_cnstr_matrix_σi_σj = @view inv_cnstr_matrix[LowCohomologySOS.KroneckerDelta{length(S)}(σi, σj)]
+                for l in 1:3
+                    e, f = rand(1:length(half_basis)), rand(1:length(half_basis))
+                    σe, σf = half_basis_idies[SAutF₂(word(half_basis[e])^action.perms[σ])], 
+                             half_basis_idies[SAutF₂(word(half_basis[f])^action.perms[σ])]
+                    @test inv_cnstr_matrix_σi_σj[σe,σf] == inv_cnstr_matrix_i_j[e,f]
+                end
+            end
+        end
+    end
 
     M = [i ≠ j ? zero(ℝSAutF₂_star) : one(ℝSAutF₂_star)+ℝSAutF₂_star(S[2]) for i in 1:length(S), j in 1:length(S)]
     order_unit = [i ≠ j ? zero(ℝSAutF₂_star) : one(ℝSAutF₂_star) for i in 1:length(S), j in 1:length(S)]
