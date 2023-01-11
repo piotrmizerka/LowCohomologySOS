0bb2c0afc7a3c93e52c3bdeac96130b855f0c5da
piotrmizerka <piotrmizerka@gmail.com>
2023-01-11 12:17:53 +0000

    induction script hopefully more readible
    embedding and symmetrization stuff as package elements now

--- scripts/induction_sl3_sl4.jl
+++ scripts/induction_sl3_sl4.jl
@@ -3,6 +3,45 @@
 using Revise 
 
 using StarAlgebras
+using Groups
+using LowCohomologySOS
+
+const N = 3
+const M = 4
+
+i = LowCohomologySOS.sln_slm_embedding(N, M)
+
+slN = i.source
+slM = i.target
+
+const half_radius = 2
+
+slN_Δ₁, slN_Iₙ, slN_Δ₁⁺, slN_Δ₁⁻ = LowCohomologySOS.sln_laplacians(slN, half_radius)
+slM_Δ₁, slM_Iₙ, slM_Δ₁⁺, slM_Δ₁⁻ = LowCohomologySOS.sln_laplacians(slM, half_radius)
+
+@assert parent(first(slM_Δ₁⁺)) == parent(first(slM_Δ₁⁻))
+
+RG_prime = parent(first(slM_Δ₁⁺))
+Δ₁_emb = LowCohomologySOS.embed_matrix(slN_Δ₁, i, RG_prime)
+
+@assert parent(first(Δ₁_emb)) == parent(first(slM_Δ₁⁻))
+
+using PermutationGroups
+
+Δ₁_emb_symmetrized = let # n = 4
+    # Σ = PermutationGroups.SymmetricGroup(n)
+    Σ = PermGroup(Perm{Int8}[perm"(1,2,3)", perm"(1,2)(3,4)"]) # alternating group A₄
+    LowCohomologySOS.weyl_symmetrize_matrix(Δ₁_emb, Σ, LowCohomologySOS._conj)
+end
+
+slM_Δ₁⁺[1,1]
+slM_Δ₁⁻[1,1]
+Δ₁_emb_symmetrized[1,1]
+
+M_ = 3*slM_Δ₁⁺+3*slM_Δ₁⁻-Δ₁_emb_symmetrized
+
+
+# the code below may be not that helpful in fact ... ###################################################
 using JuMP
 
 function alpha_beta_problem(
@@ -64,8 +103,6 @@ function alpha_beta_adjust(
     return JuMP.value.(problem[:α]), JuMP.value.(problem[:β])
 end
 
-using Groups
-
 # Code below intended for tests ###############################################################
 function cyclic_group(n::Integer)
     A = Alphabet([:a, :A], [2, 1])
@@ -86,170 +123,12 @@ RG =  LowCohomologySOS.group_ring(G, 1)
 a, = Groups.gens(G)
 A = [one(RG) RG(a); one(RG) one(RG)]
 B = [one(RG) zero(RG); zero(RG) one(RG)]
-M = [2*one(RG) RG(a); one(RG) 2*one(RG)]
+Mx= [2*one(RG) RG(a); one(RG) 2*one(RG)]
 
-alpha_beta_adjustx = alpha_beta_adjust(A, B, M)
-alpha_beta_adjustx = alpha_beta_adjust(A, B, M, false)
+alpha_beta_adjustx = alpha_beta_adjust(A, B, Mx)
+alpha_beta_adjustx = alpha_beta_adjust(A, B, Mx, false)
 #############################################################################################
-
-using LowCohomologySOS
-
-const N = 4
-
-i = LowCohomologySOS.sln_slm_embedding(3, N)
-
-sl3 = i.source
-slN = i.target
-
-const half_radius = 2
-
-sl3_Δ₁, sl3_Iₙ, sl3_Δ₁⁺, sl3_Δ₁⁻ = LowCohomologySOS.sln_laplacians(sl3, half_radius)
-Δ₁, Iₙ, Δ₁⁺, Δ₁⁻ = LowCohomologySOS.sln_laplacians(slN, half_radius)
-
-function laplacian_embedding(n::Integer, m::Integer)
-    # TODO ??
-end
-
-# Laplacian embedding from SL(3,Z) to SL(4,Z) ###############################################
-@assert parent(first(Δ₁⁺)) == parent(first(Δ₁⁻))
-
-RG_prime = parent(first(Δ₁⁺))
-
-Δ₁_emb = LowCohomologySOS.embed_matrix(sl3_Δ₁, i, RG_prime)
-
-@assert parent(first(Δ₁_emb)) == parent(first(Δ₁⁻))
-#############################################################################################
-
-using SymbolicWedderburn
-using SparseArrays
-
-function act_on_matrix(
-    M::AbstractMatrix{<:AlgebraElement}, # M has to be square and indexed by the generators
-    σ::Groups.GroupElement,
-    act::LowCohomologySOS.AlphabetPermutation
-)
-    RG = parent(first(M))
-    G = parent(first(basis(RG)))
-    S = gens(G)
-    gen_idies = Dict(S[i] => i for i in eachindex(S))
-    basis_ = basis(RG)
-
-    result = [zero(RG) for i in eachindex(S), j in eachindex(S)]
-
-    for i in eachindex(S)
-        for j in eachindex(S)
-            s, t = S[i], S[j]
-            s_σ_inv, t_σ_inv = SymbolicWedderburn.action(act, σ^(-1), s), SymbolicWedderburn.action(act, σ^(-1), t)
-            # s_σ_inv, t_σ_inv = SymbolicWedderburn.action(act, σ, s), SymbolicWedderburn.action(act, σ, t) # for left action
-            coeffs_ = coeffs(M[gen_idies[s_σ_inv],gen_idies[t_σ_inv]])
-            for ind in SparseArrays.nonzeroinds(coeffs_)
-                result[i,j] += coeffs_[ind]*RG(SymbolicWedderburn.action(act, σ, basis_[ind]))
-                # result[i,j] += coeffs_[ind]*RG(SymbolicWedderburn.action(act, σ^(-1), basis_[ind])) # for left action
-            end
-        end
-    end
-
-    return result
-end
-
-using PermutationGroups
-
-function alphabet_permutation(
-    A::Alphabet, 
-    G, 
-    op,
-    n::Integer
-)
-    return LowCohomologySOS.AlphabetPermutation(
-        Dict(
-            g => PermutationGroups.Perm([A[op(l, g, n)] for l in A.letters]) for
-            g in G
-        ),
-    )
-end
-
-function sln_alphabet_op(
-    l,
-    σ::PermutationGroups.AbstractPerm,
-    n::Integer
-)
-    return Groups.MatrixGroups.ElementaryMatrix{n}(l.i^σ, l.j^σ, l.val)
-end
-
-# Code below intended for tests (change @assert to @test) ###############################################################
-const N = 2
-
-sln = SL(N,Int8)
-Σ = PermutationGroups.SymmetricGroup(N)
-alphabet_permutation_ = alphabet_permutation(alphabet(sln), Σ, sln_alphabet_op, N)
-
-RG =  LowCohomologySOS.group_ring(sln, 1)
-e12, e21 = gens(sln)
-M = [RG(e12) zero(RG); RG(e21) one(RG)]
-
-σ = collect(Σ)[2]
-
-m_σ = act_on_matrix(M, σ, alphabet_permutation_)
-
-# action definition agreement:
-const n = 3
-
-sln = SL(n,Int8)
-Σ = PermutationGroups.SymmetricGroup(n)
-alphabet_permutation_ = alphabet_permutation(alphabet(sln), Σ, sln_alphabet_op, n)
-
-RG =  LowCohomologySOS.group_ring(sln, 1)
-e12, e13, e21, e23, e31, e32 = gens(sln)
-M = [
-    RG(e12) zero(RG) zero(RG) zero(RG) zero(RG) zero(RG);
-    zero(RG) zero(RG) zero(RG) zero(RG) zero(RG) zero(RG); 
-    zero(RG) zero(RG) zero(RG) zero(RG) zero(RG) zero(RG);
-    zero(RG) zero(RG) zero(RG) zero(RG) zero(RG) zero(RG);
-    zero(RG) zero(RG) zero(RG) zero(RG) zero(RG) zero(RG); 
-    zero(RG) zero(RG) zero(RG) zero(RG) zero(RG) zero(RG)
-]
-
-@assert act_on_matrix(M, one(Σ), alphabet_permutation_) == M
-for σ in Σ
-    for τ in Σ
-        @assert act_on_matrix(M, σ*τ, alphabet_permutation_) == act_on_matrix(act_on_matrix(M, σ, alphabet_permutation_), τ, alphabet_permutation_)
-        # @assert act_on_matrix(M, σ*τ, alphabet_permutation_) == act_on_matrix(act_on_matrix(M, τ, alphabet_permutation_), σ, alphabet_permutation_) # for left action
-    end
-end
-#############################################################################################
-
-function weyl_symmetrize_matrix(
-    M::AbstractMatrix{<:AlgebraElement},
-    Σ, # the symmetry group (either symmetric group or wreath product)
-    op,
-    n::Integer
-)
-    RG = parent(first(M))
-    G = parent(first(basis(RG)))
-    S = gens(G)
-
-    alphabet_permutation_ = alphabet_permutation(alphabet(G), Σ, op, n)
-
-    result = [zero(RG) for i in eachindex(S), j in eachindex(S)]
-    for σ in Σ
-        result += act_on_matrix(M, σ, alphabet_permutation_)
-    end
-
-    return result
-end
-
-Δ₁_emb_symmetrized = let n = 4
-    # Σ = PermutationGroups.SymmetricGroup(n)
-    Σ = PermGroup(Perm{Int8}[perm"(1,2,3)", perm"(1,2)(3,4)"]) # alternating group A₄
-    weyl_symmetrize_matrix(Δ₁_emb, Σ, sln_alphabet_op, n)
-end
-
 # The last functions to call:
-alpha_beta_adjust(Δ₁⁺, Δ₁⁻, Δ₁_emb_symmetrized)
-alpha_beta_adjust(Δ₁⁺, Δ₁⁻, Δ₁_emb_symmetrized, false)
-
-Δ₁⁺[1,1]
-Δ₁⁻[1,1]
-Δ₁_emb_symmetrized[1,1]
-
-M = 3*Δ₁⁺+3*Δ₁⁻-Δ₁_emb_symmetrized
+alpha_beta_adjust(slM_Δ₁⁺, slM_Δ₁⁻, Δ₁_emb_symmetrized)
+alpha_beta_adjust(slM_Δ₁⁺, slM_Δ₁⁻, Δ₁_emb_symmetrized, false)
+###################################################################################################
