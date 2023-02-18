0b769293850d57d2858b1f2c02ee782e2c228b14
piotrmizerka <piotrmizerka@gmail.com>
2023-02-18 16:09:47 +0000

    adj part for sl3 without Wedderburn

--- scripts/SL_3_Z_Delta_1.jl
+++ scripts/SL_3_Z_Delta_1.jl
@@ -8,7 +8,7 @@ using JuMP
 include(joinpath(@__DIR__, "optimizers.jl"))
 include(joinpath(@__DIR__, "utils.jl"))
 
-Δ₁, Iₙ, half_basis = let half_radius = 2
+M, Iₙ, half_basis = let half_radius = 2
     SL(n, R) = MatrixGroups.SpecialLinearGroup{n}(R)
     SL₃ℤ = SL(3, Int8)
 
@@ -26,11 +26,17 @@ include(joinpath(@__DIR__, "utils.jl"))
 
     relations = [
         e12 * e13 * e12^(-1) * e13^(-1),
-        e12 * e32 * e12^(-1) * e32^(-1),
-        e13 * e23 * e13^(-1) * e23^(-1),
+        e13 * e12 * e13^(-1) * e12^(-1),
+        e21 * e23 * e21^(-1) * e23^(-1),
         e23 * e21 * e23^(-1) * e21^(-1),
-        e21 * e31 * e21^(-1) * e31^(-1),
         e31 * e32 * e31^(-1) * e32^(-1),
+        e32 * e31 * e32^(-1) * e31^(-1),
+        e21 * e31 * e21^(-1) * e31^(-1),
+        e31 * e21 * e31^(-1) * e21^(-1),
+        e12 * e32 * e12^(-1) * e32^(-1),
+        e32 * e12 * e32^(-1) * e12^(-1),
+        e13 * e23 * e13^(-1) * e23^(-1),
+        e23 * e13 * e23^(-1) * e13^(-1),
         e12 * e23 * e12^(-1) * e23^(-1) * e13^(-1),
         e13 * e32 * e13^(-1) * e32^(-1) * e12^(-1),
         e21 * e13 * e21^(-1) * e13^(-1) * e23^(-1),
@@ -40,20 +46,20 @@ include(joinpath(@__DIR__, "utils.jl"))
     ]
 
     Δ₁, Iₙ, Δ₁⁺, Δ₁⁻ = LowCohomologySOS.spectral_gap_elements(quotient_hom, relations, half_basis)
-    Δ₁, Iₙ, half_basis
+    sq, adj, op = LowCohomologySOS.sq_adj_op(Δ₁⁻,gens(SL₃ℤ))
+    Δ₁⁺+adj, Iₙ, half_basis
 end
 
 SL₃ℤ_data = (
-    M = Δ₁,
+    M = M,
     order_unit = Iₙ,
-    half_basis = half_basis,
-    RG = parent(first(Δ₁)),
+    half_basis = half_basis
 )
 
-Δ₁_sos_problem = LowCohomologySOS.sos_problem(Δ₁, Iₙ)
+sos_problem = LowCohomologySOS.sos_problem(Δ₁, Iₙ, 0.22)
 
 solve_in_loop(
-    Δ₁_sos_problem,
+    sos_problem,
     logdir = "./logs",
     optimizer = scs_opt(eps = 1e-9, max_iters = 20_000),
     data = SL₃ℤ_data
