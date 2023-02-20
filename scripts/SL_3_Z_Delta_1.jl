eea1c5f8f70ee81b9dcd5543391c55e10ec42beb
piotrmizerka <piotrmizerka@gmail.com>
2023-02-20 11:25:39 +0000

    keep the original computation for Delta_1 for sl3z

--- scripts/SL_3_Z_Delta_1.jl
+++ scripts/SL_3_Z_Delta_1.jl
@@ -8,7 +8,7 @@ using JuMP
 include(joinpath(@__DIR__, "optimizers.jl"))
 include(joinpath(@__DIR__, "utils.jl"))
 
-M, Iₙ, half_basis = let half_radius = 2
+Δ₁, Iₙ, half_basis = let half_radius = 2
     SL(n, R) = MatrixGroups.SpecialLinearGroup{n}(R)
     SL₃ℤ = SL(3, Int8)
 
@@ -26,17 +26,11 @@ M, Iₙ, half_basis = let half_radius = 2
 
     relations = [
         e12 * e13 * e12^(-1) * e13^(-1),
-        e13 * e12 * e13^(-1) * e12^(-1),
-        e21 * e23 * e21^(-1) * e23^(-1),
-        e23 * e21 * e23^(-1) * e21^(-1),
-        e31 * e32 * e31^(-1) * e32^(-1),
-        e32 * e31 * e32^(-1) * e31^(-1),
-        e21 * e31 * e21^(-1) * e31^(-1),
-        e31 * e21 * e31^(-1) * e21^(-1),
         e12 * e32 * e12^(-1) * e32^(-1),
-        e32 * e12 * e32^(-1) * e12^(-1),
         e13 * e23 * e13^(-1) * e23^(-1),
-        e23 * e13 * e23^(-1) * e13^(-1),
+        e23 * e21 * e23^(-1) * e21^(-1),
+        e21 * e31 * e21^(-1) * e31^(-1),
+        e31 * e32 * e31^(-1) * e32^(-1),
         e12 * e23 * e12^(-1) * e23^(-1) * e13^(-1),
         e13 * e32 * e13^(-1) * e32^(-1) * e12^(-1),
         e21 * e13 * e21^(-1) * e13^(-1) * e23^(-1),
@@ -45,21 +39,20 @@ M, Iₙ, half_basis = let half_radius = 2
         e32 * e21 * e32^(-1) * e21^(-1) * e31^(-1),
     ]
 
-    Δ₁, Iₙ, Δ₁⁺, Δ₁⁻ = LowCohomologySOS.spectral_gap_elements(quotient_hom, relations, half_basis)
-    sq, adj, op = LowCohomologySOS.sq_adj_op(Δ₁⁻,gens(SL₃ℤ))
-    Δ₁⁺+adj, Iₙ, half_basis
+    Δ₁, Iₙ = LowCohomologySOS.spectral_gap_elements(quotient_hom, relations, half_basis)
+    Δ₁, Iₙ, half_basis
 end
 
 SL₃ℤ_data = (
-    M = M,
+    M = Δ₁,
     order_unit = Iₙ,
     half_basis = half_basis
 )
 
-sos_problem = LowCohomologySOS.sos_problem(Δ₁, Iₙ, 0.22)
+Δ₁_sos_problem = LowCohomologySOS.sos_problem(Δ₁, Iₙ)
 
 solve_in_loop(
-    sos_problem,
+    Δ₁_sos_problem,
     logdir = "./logs",
     optimizer = scs_opt(eps = 1e-9, max_iters = 20_000),
     data = SL₃ℤ_data
