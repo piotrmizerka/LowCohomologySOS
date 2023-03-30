0edf0e00d6efed5cdc2590348f7aa7e5db492085
piotrmizerka <piotrmizerka@gmail.com>
2023-03-30 18:38:10 +0100

    sautf4 for adj and polishing up

--- scripts/SAut_F_4_exp.jl
+++ scripts/SAut_F_4_exp.jl
@@ -4,30 +4,26 @@ Pkg.activate(normpath(joinpath(@__DIR__, "../")))
 using LinearAlgebra
 ENV["JULIA_NUM_THREADS"] = Sys.CPU_THREADS÷2
 LinearAlgebra.BLAS.set_num_threads(Sys.CPU_THREADS÷2)
-
 using LowCohomologySOS
 using Groups
 using JuMP
 using SparseArrays
+using PermutationGroups
+using SymbolicWedderburn
+using IntervalArithmetic
 include(joinpath(@__DIR__, "optimizers.jl"))
 include(joinpath(@__DIR__, "utils.jl"))
 
+# Compute Jacobian in the free group ring
 const N = 4
-
 sautfN = Groups.SpecialAutomorphismGroup(FreeGroup(N))
 F_G = FreeGroup(alphabet(sautfN))
 S = gens(sautfN)
-
-# relations_op = LowCohomologySOS.relations(sautfN, F_G, S, true, N, "op")
-# relations_sq = LowCohomologySOS.relations(sautfN, F_G, S, true, N, "sq")
-# relations = union(relations_op,relations_sq)
-relations = LowCohomologySOS.relations(sautfN, F_G, S, true, N, "all")
-
+relations = LowCohomologySOS.relations(sautfN, F_G, S, true, N, "adj")
 d₁ = LowCohomologySOS.jacobian_matrix(relations, gens(F_G))
 
-S_inv = let s = gens(sautfN)
-    [s; inv.(s)]
-end
+# Compute the differentials supported on ball of readius 2 in the standard gen set
+S_inv = [S; inv.(S)]
 half_basis, sizes = Groups.wlmetric_ball(S_inv, radius = 2)
 RG = LowCohomologySOS.group_ring(sautfN, half_basis, star_multiplication = false)
 quotient_hom = let source = F_G, target = sautfN
@@ -35,6 +31,8 @@ quotient_hom = let source = F_G, target = sautfN
 end
 d0x = LowCohomologySOS.embed.(Ref(quotient_hom), LowCohomologySOS.d₀(parent(first(d₁)), gens(F_G)), Ref(RG))
 d1x = LowCohomologySOS.embed.(Ref(quotient_hom), d₁, Ref(RG))
+
+# Restrict attention to the differentials' support
 half_basis_restr = [one(sautfN)]
 for j in eachindex(d0x)
     for i in SparseArrays.nonzeroinds(d0x[j].coeffs)
@@ -47,51 +45,36 @@ for j in eachindex(d1x)
     end
 end
 half_basis_restr = unique!([half_basis_restr; inv.(half_basis_restr)])
-RGx = LowCohomologySOS.group_ring(sautfN, half_basis_restr, star_multiplication = false)
-d0xx = LowCohomologySOS.embed.(identity, d0x, Ref(RGx))
-d1xx = LowCohomologySOS.embed.(identity, d1x, Ref(RGx))
-Δ1⁺x = d1xx' * d1xx
-Δ1⁻x = d0xx * d0xx'
-RG_star = LowCohomologySOS.group_ring(sautfN, half_basis_restr, star_multiplication = true)
 
-Δ1⁺ = LowCohomologySOS.embed.(identity, Δ1⁺x, Ref(RG_star))
-Δ1⁻ = LowCohomologySOS.embed.(identity, Δ1⁻x, Ref(RG_star))
-Δ1 = Δ1⁺+Δ1⁻
-I = [i ≠ j ? zero(parent(first(Δ1⁺))) : one(parent(first(Δ1⁺))) for i in 1:length(d0x), j in 1:length(d0x)]
+# Compute Laplacians over RG over the differentials' support
+Δ1, I, Δ1⁺, Δ1⁻ = LowCohomologySOS.laplacians(sautfN, half_basis_restr, S, sq_adj_op_ = "adj")
+RG_star = parent(first(Δ1))
+I = [i ≠ j ? zero(RG_star) : one(RG_star) for i in 1:length(d0x), j in 1:length(d0x)]
+sq, adj, op = LowCohomologySOS.sq_adj_op(Δ1⁻, S)
+Adj = Δ1⁺+adj
 
+# Symmetrize the problem using Wedderburn to accelerate the computations ##############
 function wedderburn_data(basis, half_basis, S)
     Σ = PermutationGroups.SymmetricGroup(N)
     actions = LowCohomologySOS.WedderburnActions(alphabet(parent(first(S))), Σ, LowCohomologySOS._conj, S, basis)
     constraints_basis, psd_basis = LowCohomologySOS.matrix_bases(basis, half_basis, S)
     return constraints_basis, psd_basis, Σ, actions
 end
-
-using PermutationGroups
-
 constraints_basis, psd_basis, Σ, action = wedderburn_data(RG_star.basis, half_basis_restr, S)
-
+# there is no point of finding a solution if we don't provide invariant matrix
 for σ in Σ
-    @assert LowCohomologySOS.act_on_matrix(Δ1, σ, action.alphabet_perm, S) == Δ1
+    @assert LowCohomologySOS.act_on_matrix(Adj, σ, action.alphabet_perm, S) == Adj
     @assert LowCohomologySOS.act_on_matrix(I, σ, action.alphabet_perm, S) == I
 end
+w_dec_matrix = SymbolicWedderburn.WedderburnDecomposition(Float64, Σ, action, constraints_basis, psd_basis)
+#################
 
-using SymbolicWedderburn
+# Find a numerical spectral gap
+sos_problem, P = LowCohomologySOS.sos_problem(Adj, I, w_dec_matrix)
+JuMP.set_optimizer(sos_problem, scs_opt(eps = 1e-9, max_iters = 100_000))
+# JuMP.set_optimizer(sos_problem, cosmo_opt(eps = 1e-7, max_iters = 30_000))
+JuMP.optimize!(sos_problem)
 
-@time begin
-    @info "Wedderburn:"
-    w_dec_matrix = SymbolicWedderburn.WedderburnDecomposition(Float64, Σ, action, constraints_basis, psd_basis)
-end
-
-@time begin
-    sos_problem = LowCohomologySOS.sos_problem(
-        Δ1, 
-        I,
-        w_dec_matrix,
-        0.05
-    )
-end
-
-JuMP.set_optimizer(sos_problem[1], scs_opt(eps = 1e-9, max_iters = 20_000))
-JuMP.optimize!(sos_problem[1])
-λ, Q = LowCohomologySOS.get_solution(sos_problem[1],sos_problem[2],w_dec_matrix)
-LowCohomologySOS.certify_sos_decomposition(Δ1, I, λ, Q, half_basis_restr)
+# Certify the numerical estimate
+λ, Q = LowCohomologySOS.get_solution(sos_problem, P, w_dec_matrix)
+LowCohomologySOS.certify_sos_decomposition(Adj, I, λ, Q, half_basis_restr)
